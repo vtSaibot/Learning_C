dbc8c65317ecb239175a2672458d5a2795db3e28
vtSaibot <145202239+vtSaibot@users.noreply.github.com>
2024-07-07 19:56:00 +0200

    sync data between mac/win

---
+++ C_constants.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    // constant = fixed value that cannot be altered troughout the execution of the program
+
+    const float PI = 3.14159;
+
+    printf("%.5f", PI);
+
+    return 0;
+}
